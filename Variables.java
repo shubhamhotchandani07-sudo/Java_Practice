e3faf71f90407d35821abb8eecf322cdadd05d4b
S <shubhamhotchandani07@gmail.com>
2026-08-08 05:47:31 -0700

    Variables In Java

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables{
+    public static void main(String[] args) {
+        System.out.println("The Sum OF Numbers Is");
+
+        //ADD
+        int a=10;
+        int b=40;
+        System.out.println(a+b);
+
+        //Subtract
+        int c=50;
+        int d=34;
+        System.out.println(c-d);
+    }
+}
