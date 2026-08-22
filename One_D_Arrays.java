eeb92de44e052bafd0a766f485293c6518ee69d9
S <shubhamhotchandani07@gmail.com>
2026-08-21 21:41:05 -0700

    Arrays In Java

---
+++ One_D_Arrays.java
@@ -0,0 +1,283 @@
+public class One_D_Arrays {
+    // public static void main(String[] args) {
+    //     int [] Marks = new int[3];
+    //     Marks[0]=97;
+    //     Marks[1]=99;
+    //     Marks[2]=93;
+    //     //1st Method Without Loop
+    //     System.out.print("Marks in Physics : ");
+    //     System.out.println(Marks[0]);
+    //     System.out.print("Marks in English : ");
+    //     System.out.println(Marks[1]);
+    //     System.out.print("Marks in Maths : ");
+    //     System.out.println(Marks[2]);
+
+    //     //2nd Method with loop
+    //     for(int i=0;i<=3;i++){
+    //         System.out.println(Marks[i]);
+    //     }
+
+    // }
+
+
+    // Ek int[] numbers array banao aur usme ye values store karo:
+    // public static void main(String[] args) {
+    //      int number[]=new int[3];
+    //      number[0]=20;
+    //      number[1]=30;
+    //      number[2]=80;
+
+    //      //without loops
+    //      System.out.println(number[0]);
+    //      System.out.println(number[1]);
+    //      System.out.println(number[2]);
+
+
+    //      //with loops
+    //      for(int i=0;i<3;i++){
+    //         System.out.println(number[i]);
+    //      }
+    // }
+//    public static void main(String[] args) {
+//         int [] Marks = new int[3];
+//         Marks[0]=97;
+//         Marks[1]=99;
+//         Marks[2]=93;
+//         //1st Method Without Loop
+//         System.out.print("Marks in Physics : ");
+//         System.out.println(Marks[0]);
+//         System.out.print("Marks in English : ");
+//         System.out.println(Marks[1]);
+//         System.out.print("Marks in Maths : ");
+//         System.out.println(Marks[2]);
+
+//         //2nd Method with loop
+//         for(int i=0;i<=3;i++){
+//             System.out.println(Marks[i]);
+//         }
+
+//     }
+
+    //Ek int[] array age banao jisme 4 ages store karo:
+    // public static void main(String[] args) {
+    //    int age[]=new int[4];
+    //    age[0]=12;
+    //    age[1]=89;
+    //    age[2]=75;
+    //    age[3]=56;
+
+    //    System.out.println(age[0]);
+    //    System.out.println(age[1]);
+    //    System.out.println(age[2]);
+    //    System.out.println(age[3]);
+
+    //    for(int i=0;i<4;i++){
+    //     System.out.println(age[i]);
+    //    }
+    // }
+
+    //Ek int[] array price banao jisme 3 prices store karo:
+    // public static void main(String[] args) {
+    //    int price[]=new int[4];
+    //    price[0]=123;
+    //    price[1]=893;
+    //    price[2]=753;
+    //    price[3]=561;
+
+    //    System.out.println("Price Of Jeans :");
+    //    System.out.println(price[0]);
+    //    System.out.println("Price Of Shirt :");
+    //    System.out.println(price[1]);
+    //    System.out.println("Price Of Lower :");
+    //    System.out.println(price[2]);
+    //    System.out.println("Price Of capri :");
+    //    System.out.println(price[3]);
+
+    //    System.out.println("***************************************************************************************" );///for diffrentiate
+    //    for(int i=0;i<4;i++){
+    //     System.out.println(price[i]);
+    //    }
+    // }
+
+    //Array ke saare elements ka sum nikalo.
+    // public static void main(String[] args) {
+    //     int[] a = new int[4];
+    //     a[0]=10;
+    //     a[1]=20;
+    //     a[2]=3;
+    //     a[3]=5;
+
+    //     int sum=0;
+    //     for(int i=0;i<4;i++){
+    //         sum=sum+a[i];
+    //     }
+    //     System.out.print("Total sum is : ");
+    //     System.out.println(sum);
+    // }
+
+    //Array me maximum element find karna hai.
+//     public static void main(String[] args) {
+
+//     int[] a = new int[4];
+//     a[0] = 10;
+//     a[1] = 20;
+//     a[2] = 3;
+//     a[3] = 5;
+
+//     int max = a[0];
+
+//     for (int i = 1; i < 4; i++) {
+//         if (a[i] > max) {
+//             max = a[i];
+//         }
+//     }
+
+//     System.out.println("Maximum is : " + max);
+// }
+
+
+    //Find minimum element find karna hai
+//     public static void main(String[] args) {
+
+//     int[] a = new int[4];
+//     a[0] = 10;
+//     a[1] = 20;
+//     a[2] = 3;
+//     a[3] = 5;
+
+//     int min = a[0];
+
+//     for (int i = 1; i < 4; i++) {
+//         if (a[i] < min) {
+//             min = a[i];
+//         }
+//     }
+
+//     System.out.println("Minimum is : " + min);
+// }
+
+    // public static void main(String[] args) {
+    //     int [] a={1,2,3,4,5,6,7,8};
+    //     int count=0;
+
+    //     for(int i=0;i<a.length;i++){
+    //         if (a[i]%2!=0) {
+    //             count+=1;
+    //         }
+    //     }
+    //     System.out.print("Total Odd Number is : ");
+    //     System.out.println(count);
+    // }
+
+    // public static void main(String[] args) {
+    //     int [] a={1,2,3,4,5,6,7,8};
+    //     int count=0;
+
+    //     for(int i=0;i<a.length;i++){
+    //         if (a[i]%2==0) {
+    //             count+=1;
+    //         }
+    //     }
+    //     System.out.print("Total Even Number is : ");
+    //     System.out.println(count);
+    // }
+
+    //Search an element
+    // public static void main(String[] args) {
+    //     int [] a = {1,2,3,4,5,6,7,8};
+
+    //     for(int i=0;i<a.length;i++){
+    //         if (a[i]==10) {
+    //             System.out.println("Found");
+    //         }
+    //         else{
+    //             System.out.println("not Found");
+    //         }
+    //     }
+    // }
+
+    //Average of Array
+    // public static void main(String[] args) {
+    //     int [] a = new int[4];
+    //     a[0]=5;
+    //     a[1]=5;
+    //     a[2]=5;
+    //     a[3]=5;
+
+    //     int total=(a[0]+a[1]+a[2]+a[3]);
+    //         int minus=total/a.length;
+
+    //         int average=minus;
+    //         System.out.println(average);
+    // }
+
+    //Positive, Negative aur Zero count
+    // public static void main(String[] args) {
+    //     int [] a = {1,2,-3,0,-1,9};
+
+    //     int positive_count=0;
+    //     int negative_count=0;
+    //     int zero_count=0;
+
+    //     for(int i=0;i<a.length;i++){
+    //         if (a[i]>0) {
+    //             positive_count+=1;
+    //         }
+    //         else if (a[i]<0) {
+    //             negative_count+=1;
+    //         }
+    //         else if (a[i]==0) {
+    //             zero_count+=1;
+    //         }
+    //         else{
+    //             System.out.println("Invalid Entered");
+    //         }
+    //     }
+    //     System.out.println(positive_count);
+    //     System.out.println(negative_count);
+    //     System.out.println(zero_count);
+    // }
+
+    //Array me kitne numbers 5 se greater hain?
+    // public static void main(String[] args) {
+    //     int [] a = {1,2,3,4,6,7,8,9};
+
+    //     int count=0;
+    //     for(int i=0;i<a.length;i++){
+    //         if (a[i]>5) {
+    //             count+=1;
+    //         }
+    //     }
+    //     System.out.println(count);
+    // }
+
+        //Array ke saare elements ko reverse order me print karo
+    //     public static void main(String[] args) {
+    //     int [] a = {1,2,3,4,6,7,8,9};
+
+    //     for(int i=7;i>=0;i--){
+    //         System.out.println(a[i]);
+    //     }
+    // }
+
+    //Array me sabse chhota positive number count karo
+    public static void main(String[] args) {
+        int [] a = {1,2,-3,0,-1,9};
+
+    int positive_count=0;
+    int min=a[0];
+    for(int i=0;i<a.length;i++){
+        if (a[i]>0 ) {
+            positive_count+=1;
+            
+        }
+        else if (a[i]>min) {
+            min=a[i];
+        }
+        
+    
+    }
+    System.out.println(positive_count);
+    System.out.println(min);
+}
+}
