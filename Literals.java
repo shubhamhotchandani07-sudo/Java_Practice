ace821f2281f8cb2bca74eef9713b7677a88fd46
S <shubhamhotchandani07@gmail.com>
2026-08-25 21:26:01 -0700

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,15 @@
+public class Literals {
+    public static void main(String[] args) {
+        //Literals --> A Constant Value Which Is Stored in a Variables
+        int age = 42;
+        System.out.println(age);
+
+        //This 42 is called a Literal Because It Having A Constant Value == "42"
+
+        short age2=43;
+        System.out.println(age2);
+         
+        String str="Shubham";
+        System.out.println(str);
+    }
+}
