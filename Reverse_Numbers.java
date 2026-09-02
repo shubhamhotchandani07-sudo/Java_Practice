17017d32e69f72d92fabdace9626a6bc01a886b8
S <shubhamhotchandani07@gmail.com>
2026-09-01 23:13:59 -0700

    Reverse Of Numbers

---
+++ Reverse_Numbers.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Reverse_Numbers {
+    // public static void main(String[] args) {
+    //     int no=1234;
+    //     int rem;
+    //     int rev=0;
+    //     while (no!=0) {
+    //         rem=no%10;
+    //         rev=rev*10+rem;
+    //         no=no/10;
+    //     }
+    //     System.out.println(rev);
+    // }
+
+    //through input taken by user for repeating
+    // public static void main(String[] args) {
+    // Scanner sc=new Scanner(System.in);
+    // System.out.print("Enter a Number Which You Want To Reverse  : ");
+    // int no=sc.nextInt();
+
+    // int rem;
+    // int rev=0;
+    // while(no!=0){
+    //     rem=no%10;
+    //     rev=rev*10+rem;
+    //     no=no/10;
+    // }
+    // System.out.println(rev);
+    // }
+
+    
+
+}
